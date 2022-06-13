c437b0adc8e775099c10df0cc642471ca84c4573
Mohammad Nedaei <nedaei2002@gmail.com>
2022-06-13 19:17:43 +0430

    feat(core): Add personal site links

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Constants} from "./utils/Constants";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={Constants.imgProfile}
+                    className="App-logo"
+                    alt="logo"/>
+                <p className={"paragraph"}>
+                    My name is Morteza Nedaei. This is my personal site. I am a Software Engineer.
+                    You can find me in the following links
+                </p>
+
+                <div className='links'>
+                    <a
+                        className="linkStyle"
+                        href="https://linkedin.com/in/morteza-nedaei"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        LinkedIn
+                    </a>
+                    <a
+                        className="linkStyle"
+                        href="https://github.com/mortezanedaei"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Github
+                    </a>
+                    <a
+                        className="linkStyle"
+                        href="https://twitter.com/MortezaNedaei"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Twitter
+                    </a>
+                </div>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/utils/Constants.ts
@@ -0,0 +1,3 @@
+export const Constants = {
+    imgProfile: 'https://media-exp1.licdn.com/dms/image/C4E03AQGGt3S3V4ryNg/profile-displayphoto-shrink_800_800/0/1576662776685?e=1660780800&v=beta&t=0I3Ud6nFPo6uRvgVZGAXGwcdZxHsZHrlsC5YZjERvns'
+}
