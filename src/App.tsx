8349b3fd49880e9d00ee7b1d14d286a71dc1e58e
Morteza Nedaei <nedaei1375@gmail.com>
2023-07-03 01:23:09 +0330

    feat: add notification permission

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import AboutSection from "./sections/about";
-import {initializeOneSignal} from "./OneSignal";
+import {initializeNotification} from "./NotificationService";
 
 function App() {
 
     useEffect(() => {
-        initializeOneSignal()
+        initializeNotification()
     }, [])
 
     return (
---
+++ src/NotificationService.ts
@@ -0,0 +1,22 @@
+export function initializeNotification(message: string = "Test Notification") {
+        // Let's check if the browser supports notifications
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+        }
+
+        // Let's check whether notification permissions have already been granted
+        else if (Notification.permission === "granted") {
+            // If it's okay let's create a notification
+            let notification = new Notification(message);
+        }
+
+        // Otherwise, we need to ask the user for permission
+        else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then(function (permission) {
+                // If the user accepts, let's create a notification
+                if (permission === "granted") {
+                    let notification = new Notification(message);
+                }
+            });
+        }
+}
