73d8c4ef99d5b6066f6e0c49a542fec135276976
Morteza Nedaei <nedaei1375@gmail.com>
2023-07-04 00:24:45 +0330

    chore: remove duplicated notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -34,15 +34,15 @@ const messaging = firebase.messaging();
 // and you should use data messages for custom notifications.
 // For more info see:
 // https://firebase.google.com/docs/cloud-messaging/concept-options
-messaging.onBackgroundMessage(function (payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+// messaging.onBackgroundMessage(function (payload) {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//     // Customize notification here
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//         body: 'Background Message body.',
+//         icon: '/firebase-logo.png'
+//     };
+//
+//     self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
